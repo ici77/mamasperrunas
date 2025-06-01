6f9d8a553cb96d1955e002ad67cb468877ad2abb
ici77 <inmacontreras77@gmail.com>
2025-06-01 19:58:52 +0200

    arreglando despliegue

--- frontend/server.js
+++ frontend/server.js
@@ -4,14 +4,15 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-// Servir archivos estáticos de Angular
+// Middleware para servir archivos estáticos de Angular
 app.use(express.static(path.join(__dirname, 'dist/frontend/browser')));
 
-// Ruta para devolver index.html si no se encuentra otro recurso
-app.get('*', (req, res) => {
- res.sendFile(path.join(__dirname, 'dist/frontend/browser/index.html'));
+// Redirigir todas las rutas al index.html (para soporte de Angular routing)
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/frontend/browser/index.html'));
 });
 
+// Iniciar servidor
 app.listen(PORT, () => {
   console.log(`Servidor funcionando en http://localhost:${PORT}`);
 });
