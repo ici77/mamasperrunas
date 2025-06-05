60bad51649efc53de5835d98859a2a0a4efa5369
ici77 <inmacontreras77@gmail.com>
2025-06-05 12:16:00 +0200

    desplegando

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://backmp-production.up.railway.app/api'
+  apiUrl: 'https://backmp-production.up.railway.app/api',
+  imagenUrlBase: 'https://backmp-production.up.railway.app/uploads/'
 };
